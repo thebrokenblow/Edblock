55c296e644c739dff86a68630b2c0836f17c5a42
Artem <artkrasov@gmail.com>
2023-09-05 23:28:27 +0300

    Добавьте файлы проекта.

---
+++ EdblockView/Edblock.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace EdblockView;
+
+/// <summary>
+/// Логика взаимодействия для Edblock.xaml
+/// </summary>
+public partial class Edblock : Window
+{
+    public Edblock()
+    {
+        InitializeComponent();
+    }
+}
