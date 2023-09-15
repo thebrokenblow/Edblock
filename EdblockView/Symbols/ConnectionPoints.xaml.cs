2ea22e8bccfdcd48c3c02c7a096d2cd2e007dee8
Artem <artkrasov@gmail.com>
2023-09-15 20:01:28 +0300

    Moved connection Point to UserControl

---
+++ EdblockView/Symbols/ConnectionPoints.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace EdblockView.Symbols;
+
+/// <summary>
+/// Логика взаимодействия для ConnectionPoints.xaml
+/// </summary>
+public partial class ConnectionPoints : UserControl
+{
+    public ConnectionPoints()
+    {
+        InitializeComponent();
+    }
+}
