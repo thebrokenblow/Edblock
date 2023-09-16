39e18bee69c2acbe7dcc95bc69d277892b16be7f
Artem <artkrasov@gmail.com>
2023-09-16 16:10:56 +0300

    Moved Scale Rectangles in User Control

---
+++ EdblockView/Symbols/ScaleRectangles.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace EdblockView.Symbols;
+
+/// <summary>
+/// Логика взаимодействия для ScaleRectangles.xaml
+/// </summary>
+public partial class ScaleRectangles : UserControl
+{
+    public ScaleRectangles()
+    {
+        InitializeComponent();
+    }
+}
