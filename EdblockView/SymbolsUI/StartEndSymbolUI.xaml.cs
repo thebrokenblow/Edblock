288e067b08533065e8dc1a85ebb65d67476cb228
Artem <artkrasov@gmail.com>
2023-12-21 17:36:09 +0300

    Added StartEndSymbolUI

---
+++ EdblockView/SymbolsUI/StartEndSymbolUI.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace EdblockView.SymbolsUI;
+
+/// <summary>
+/// Логика взаимодействия для StartEndSymbolUI.xaml
+/// </summary>
+public partial class StartEndSymbolUI : UserControl
+{
+    public StartEndSymbolUI()
+    {
+        InitializeComponent();
+    }
+}
