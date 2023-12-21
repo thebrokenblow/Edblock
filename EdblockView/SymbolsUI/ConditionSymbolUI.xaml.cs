6975e83d048c38ad086105888ff48b33370bf196
Artem <artkrasov@gmail.com>
2023-12-21 16:04:39 +0300

    Added ConditionSymbolUI

---
+++ EdblockView/SymbolsUI/ConditionSymbolUI.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace EdblockView.SymbolsUI;
+
+/// <summary>
+/// Логика взаимодействия для ConditionSymbolUI.xaml
+/// </summary>
+public partial class ConditionSymbolUI : UserControl
+{
+    public ConditionSymbolUI()
+    {
+        InitializeComponent();
+    }
+}
