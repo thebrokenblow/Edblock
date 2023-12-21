8cac9cb5d60cd386907d0a2e0c0a2f24cc101362
Artem <artkrasov@gmail.com>
2023-12-21 16:57:07 +0300

    Added HorizontalConditionSymbolUI

---
+++ EdblockView/SymbolsUI/HorizontalConditionSymbolUI.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace EdblockView.SymbolsUI;
+
+/// <summary>
+/// Логика взаимодействия для HorizontalConditionSymbolUI.xaml
+/// </summary>
+public partial class HorizontalConditionSymbolUI : UserControl
+{
+    public HorizontalConditionSymbolUI()
+    {
+        InitializeComponent();
+    }
+}
