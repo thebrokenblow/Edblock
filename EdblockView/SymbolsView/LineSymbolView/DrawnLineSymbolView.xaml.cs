9c920a7cc9a44f9fb51024e8c85af44699b15fa1
Artem <artkrasov@gmail.com>
2023-12-25 23:57:31 +0300

    fixed the errors

---
+++ EdblockView/SymbolsView/LineSymbolView/DrawnLineSymbolView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EdblockViewModel.Symbols.LineSymbols;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EdblockView.SymbolsView.LineSymbolView;
+
+/// <summary>
+/// Логика взаимодействия для DrawnLineSymbolView.xaml
+/// </summary>
+public partial class DrawnLineSymbolView : UserControl
+{
+    public DrawnLineSymbolView()
+    {
+        InitializeComponent();
+    }
+
+    private void SelectLine(object sender, MouseButtonEventArgs e)
+    {
+        var itemsControlLines = (ItemsControl)sender;
+        var drawnLineSymbolVM = (DrawnLineSymbolVM)itemsControlLines.DataContext;
+
+        drawnLineSymbolVM.SelectLine();
+
+        e.Handled = true;
+    }
+
+    private void ShowTextField(object sender, MouseButtonEventArgs e)
+    {
+        var itemsControlLines = (ItemsControl)sender;
+        var drawnLineSymbolVM = (DrawnLineSymbolVM)itemsControlLines.DataContext;
+
+        drawnLineSymbolVM.ShowTextField();
+
+        e.Handled = true;
+    }
+}
