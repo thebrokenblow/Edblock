d471986aa684946d57e0cb28fffe1ebbff01a477
Artem <artkrasov@gmail.com>
2023-12-25 23:09:48 +0300

    Revert "twst"
    This reverts commit f134875556c74ee82d69a6d0e27f0ee4a2c1ee1a.

--- EdblockView/Components/CanvasSymbols.xaml.cs
+++ EdblockView/Components/CanvasSymbols.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows.Input;
 using System.Windows.Controls;
-using System.Windows;
 
 namespace EdblockView.Components;
 
@@ -48,10 +47,5 @@ public partial class CanvasSymbols : UserControl
         //    }
         //    scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset + canvasOffset);
         //}
-    }
-
-    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-    {
-        MessageBox.Show("ПРИВЕТ");
     }
 }
