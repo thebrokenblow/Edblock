007a375a58be3cbc26afe15da3b064a882cddaf7
Artem <artkrasov@gmail.com>
2024-04-01 20:55:36 +0300

    Поправил стили

--- EdblockView/Pages/Editor.xaml.cs
+++ EdblockView/Pages/Editor.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Controls;
-using EdblockView.Abstractions;
 using EdblockViewModel.PagesVM;
 
 namespace EdblockView.Pages;
