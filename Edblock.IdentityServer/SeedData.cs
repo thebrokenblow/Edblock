23ae849a26fa76142d0e68005f3d0b83ae8d2502
Artem <artkrasov@gmail.com>
2024-05-08 19:16:13 +0300

    Добавил IdentityServer

---
+++ Edblock.IdentityServer/SeedData.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Linq;
+using IdentityModel;
+using Edblock.IdentityServer.Data;
+using Edblock.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Edblock.IdentityServer;
+
+public class SeedData
+{
+    public static void EnsureSeedData(string connectionString)
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(connectionString));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        using var serviceProvider = services.BuildServiceProvider();
+        using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+        context.Database.Migrate();
+
+        var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var alice = userMgr.FindByNameAsync("alice").Result;
+        if (alice == null)
+        {
+            alice = new()
+            {
+                UserName = "alice",
+                Email = "AliceSmith@email.com",
+                EmailConfirmed = true,
+            };
+            var result = userMgr.CreateAsync(alice, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(alice, [
+                        new(JwtClaimTypes.Name, "Alice Smith"),
+                        new(JwtClaimTypes.GivenName, "Alice"),
+                        new(JwtClaimTypes.FamilyName, "Smith"),
+                        new(JwtClaimTypes.WebSite, "http://alice.com"),
+                    ]).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("alice created");
+        }
+        else
+        {
+            Log.Debug("alice already exists");
+        }
+
+        var bob = userMgr.FindByNameAsync("bob").Result;
+        if (bob == null)
+        {
+            bob = new()
+            {
+                UserName = "bob",
+                Email = "BobSmith@email.com",
+                EmailConfirmed = true
+            };
+            var result = userMgr.CreateAsync(bob, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(bob, [
+                new(JwtClaimTypes.Name, "Bob Smith"),
+                        new(JwtClaimTypes.GivenName, "Bob"),
+                        new(JwtClaimTypes.FamilyName, "Smith"),
+                        new(JwtClaimTypes.WebSite, "http://bob.com"),
+                        new("location", "somewhere")
+            ]).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("bob created");
+        }
+        else
+        {
+            Log.Debug("bob already exists");
+        }
+    }
+}
---
+++ Edblock.IdentityServer/Startup.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4;
+using Edblock.IdentityServer.Data;
+using Edblock.IdentityServer.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Edblock.IdentityServer;
+
+public class Startup(IWebHostEnvironment environment, IConfiguration configuration)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("UsersConnection")));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        var builder = services.AddIdentityServer(options =>
+        {
+            options.Events.RaiseErrorEvents = true;
+            options.Events.RaiseInformationEvents = true;
+            options.Events.RaiseFailureEvents = true;
+            options.Events.RaiseSuccessEvents = true;
+            options.EmitStaticAudienceClaim = true;
+        })
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddAspNetIdentity<ApplicationUser>();
+
+        builder.AddDeveloperSigningCredential();
+
+        services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                options.ClientId = "copy client ID from Google here";
+                options.ClientSecret = "copy client secret from Google here";
+            });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
+}
